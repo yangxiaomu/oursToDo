be5aeb4e0d55c9d17799660b07adbe3f5e03891e
wangbo123855842 <wangbo123855842@126.com>
2015-12-16 16:48:51 +0800

    ログイン画面作成

---
+++ ios_views/login/login.ios.js
@@ -0,0 +1,137 @@
+'use strict';
+var React = require('react-native');
+var Dimensions = require('Dimensions');
+var windowSize = Dimensions.get('window');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Image
+} = React;
+
+var login = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  render: function() {
+    return (
+        <View style={styles.container}>
+            <Image style={styles.bg} source={{uri: 'http://i.imgur.com/xlQ56UK.jpg'}} />
+            <View style={styles.header}>
+                <Image style={styles.mark} source={require('../../img/icon.png')} />
+            </View>
+            <View style={styles.inputs}>
+                <View style={styles.inputContainer}>
+                    <Image style={styles.inputUsername} source={{uri: 'http://i.imgur.com/iVVVMRX.png'}}/>
+                    <TextInput 
+                        style={[styles.input, styles.whiteFont]}
+                        placeholder="Username"
+                        placeholderTextColor="#FFF"
+                        value={this.state.username}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Image style={styles.inputPassword} source={{uri: 'http://i.imgur.com/ON58SIG.png'}}/>
+                    <TextInput
+                        password={true}
+                        style={[styles.input, styles.whiteFont]}
+                        placeholder="Pasword"
+                        placeholderTextColor="#FFF"
+                        value={this.state.password}
+                    />
+                </View>
+            </View>
+            <View style={styles.signin}>
+                <Text style={styles.whiteFont}>Sign In</Text>
+            </View>
+            <View style={styles.signup}>
+                <Text style={styles.greyFont}>Welcome To OursToDo</Text>
+            </View>
+        </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+    container: {
+      flexDirection: 'column',
+      flex: 1,
+      backgroundColor: 'transparent'
+    },
+    bg: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        width: windowSize.width,
+        height: windowSize.height
+    },
+    header: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: .5,
+        backgroundColor: 'transparent'
+    },
+    mark: {
+        width: 180,
+        height: 200
+    },
+    signin: {
+        backgroundColor: '#FF3366',
+        padding: 20,
+        alignItems: 'center'
+    },
+    signup: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: .15
+    },
+    inputs: {
+        marginTop: 10,
+        marginBottom: 10,
+        flex: .25
+    },
+    inputPassword: {
+        marginLeft: 15,
+        width: 20,
+        height: 21
+    },
+    inputUsername: {
+      marginLeft: 15,
+      width: 20,
+      height: 20
+    },
+    inputContainer: {
+        padding: 10,
+        borderWidth: 1,
+        borderBottomColor: '#CCC',
+        borderColor: 'transparent'
+    },
+    input: {
+        position: 'absolute',
+        left: 61,
+        top: 12,
+        right: 0,
+        height: 20,
+        fontSize: 14
+    },
+    forgotContainer: {
+      alignItems: 'flex-end',
+      padding: 15,
+    },
+    greyFont: {
+      color: '#D8D8D8'
+    },
+    whiteFont: {
+      color: '#FFF'
+    }
+})
+
+AppRegistry.registerComponent('login', () => login);
+
+module.exports = login;
