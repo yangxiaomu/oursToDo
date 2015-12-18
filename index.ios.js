92dcd339cab9d082c9cf7cbe5bce82ff8edee851
yt <ytxiaomu@gmail.com>
2015-12-18 14:43:14 +0800

    初始画面调整为login画面

--- index.ios.js
+++ index.ios.js
@@ -1,116 +1,213 @@
-/**
- * oursToDo
- * https://github.com/yangxiaomu/oursToDo.git
- */
 'use strict';
-
 var React = require('react-native');
-var Navigation = require('./ios_views/common/navigation');
-var GroupToDo = require('./ios_views/group/todo_group_list');
-var Summary = require('./ios_views/summary/sum_group_list');
-var addGroup = require('./ios_views/group/add_group');
-
-var Icon = require("react-native-vector-icons/FontAwesome");
-
+var Dimensions = require('Dimensions');
+var windowSize = Dimensions.get('window');
+var home = require('./ios_views/common/home');
 
 var {
   AppRegistry,
   StyleSheet,
-  Text,
   View,
-  TabBarIOS,
-  Navigator,
-  ScrollView,
-  NavigatorIOS,
-  StatusBarIOS
+  Text,
+  TextInput,
+  Image,
+  AlertIOS,
+  TouchableHighlight
 } = React;
 
-StatusBarIOS.setHidden(true);
-var oursToDo = React.createClass({
-  getInitialState: function(){
+var login = React.createClass({
+  getInitialState: function() {
     return {
-      selectedTab: GroupToDo
-    };
-  },
-
-  onRightButtonPress: function() {
-    this.refs.nav.push({
-      title: 'addGroup',
-      component: addGroup
-    })
-  },
-
-  _renderContent: function(category: string, title: ?string) {
-    if (title == "GroupToDo") {
-      return (
-        <NavigatorIOS style={styles.wrapper}
-          ref="nav"
-          tintColor='#DA552F'
-          barTintColor='#FFFFFD'
-          titleTextColor='#DA552F'
-          initialRoute={{
-            component: this.state.selectedTab,
-            title: title,
-            passProps: {filter: category},
-            rightButtonTitle: "➕",
-            onRightButtonPress: this.onRightButtonPress
-          }}
-        />
-      );
-    } else {
-      return (
-        <NavigatorIOS style={styles.wrapper}
-          initialRoute={{
-            component: this.state.selectedTab,
-            title: title,
-            passProps: {filter: category}
-          }}
-        />
-      );
+      user_code: '',
+      user_pass: ''
     }
   },
-
   render: function() {
     return (
-      <TabBarIOS tintColor={"#ea4c89"}>
-
-        <Icon.TabBarItem
-          title="todo列表"
-          iconName="dribbble"
-          selectedIconName="dribbble"
-          selected={this.state.selectedTab == GroupToDo}
-          onPress={() => {
-            this.setState({
-              selectedTab: GroupToDo
-            });
-          }}>
-          {this._renderContent("GroupToDo", "GroupToDo")}
-
-        </Icon.TabBarItem>
-
-        <Icon.TabBarItem
-          title="统计"
-          iconName="lightbulb-o"
-          selectedIconName="lightbulb-o"
-          selected={this.state.selectedTab == Summary}
-          onPress={() => {
-            this.setState({
-              selectedTab: Summary
-            });
-          }}>
-          {this._renderContent("Summary", "Summary")}
-        </Icon.TabBarItem>
-        
-      </TabBarIOS>
-
+        <View style={styles.container}>
+            <Image style={styles.bg} source={{uri: 'http://i.imgur.com/xlQ56UK.jpg'}} />
+            <View style={styles.header}>
+                <Image style={styles.mark} source={require('./img/icon.png')} />
+            </View>
+            <View style={styles.inputs}>
+                <View style={styles.inputContainer}>
+                    <Image style={styles.inputUsername} source={{uri: 'http://i.imgur.com/iVVVMRX.png'}}/>
+                    <TextInput 
+                        style={[styles.input, styles.whiteFont]}
+                        placeholder="Username"
+                        placeholderTextColor="#FFF"
+                        value={this.state.user_code}
+                        onChangeText={(text) => this.setState({user_code: text})}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Image style={styles.inputPassword} source={{uri: 'http://i.imgur.com/ON58SIG.png'}}/>
+                    <TextInput
+                        password={true}
+                        style={[styles.input, styles.whiteFont]}
+                        placeholder="Pasword"
+                        placeholderTextColor="#FFF"
+                        value={this.state.user_pass}
+                        onChangeText={(text) => this.setState({user_pass: text})}
+                    />
+                </View>
+            </View>
+            <TouchableHighlight onPress={this.login}>
+              <View style={styles.signin}>
+                  <Text style={styles.whiteFont}>Sign In</Text>
+              </View>
+            </TouchableHighlight>
+            <View style={styles.signup}>
+                <Text style={styles.greyFont}>Welcome To OursToDo</Text>
+            </View>
+        </View>
     );
+  },
+
+  login: function() {
+    var user_code = this.state.user_code;
+    var user_pass = this.state.user_pass;
+    fetch('http://agc.dreamarts.com.cn/hibiki/rest/1/binders/users/views/allData/documents?user_code=' + user_code + "&user_pass=" + user_pass, {
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': make_base_auth('b_wang', 'b_wang')
+      }
+    }).then(
+      function(response) {
+        if (response.status === 401) {
+          AlertIOS.alert("Sm＠rtDB認証失敗しまいました！");
+        }
+        if (response.status === 200) {
+          var result = JSON.parse(response._bodyText);
+          if (parseInt(result.totalCount) > 0) {
+            AlertIOS.alert("ログイン成功！")
+            this.props.navigator.push({
+              component: home,
+            });
+
+          } else {
+            AlertIOS.alert("ユーザー・パスワードが無効！")
+          }
+
+        }
+      }
+    ).catch(function(err) {
+      //aAlertIOS.alert("システムエラー！");
+    });
   }
+
 });
+
+function make_base_auth(user, password) {
+  var tok = base64_encode(user + ":" + password);
+  return "Basic " + tok;
+}
+
+function base64_encode(str){
+  var c1, c2, c3;
+  var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";                
+  var i = 0, len= str.length, string = '';
+
+  while (i < len){
+    c1 = str.charCodeAt(i++) & 0xff;
+    if (i == len){
+      string += base64EncodeChars.charAt(c1 >> 2);
+      string += base64EncodeChars.charAt((c1 & 0x3) << 4);
+      string += "==";
+      break;
+    }
+    c2 = str.charCodeAt(i++);
+    if (i == len){
+      string += base64EncodeChars.charAt(c1 >> 2);
+      string += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+      string += base64EncodeChars.charAt((c2 & 0xF) << 2);
+      string += "=";
+            break;
+    }
+    c3 = str.charCodeAt(i++);
+    string += base64EncodeChars.charAt(c1 >> 2);
+    string += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+    string += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+    string += base64EncodeChars.charAt(c3 & 0x3F)
+  }
+  return string;
+}
 
 var styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-  }
-});
+    container: {
+      flexDirection: 'column',
+      flex: 1,
+      backgroundColor: 'transparent'
+    },
+    bg: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        width: windowSize.width,
+        height: windowSize.height
+    },
+    header: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: .5,
+        backgroundColor: 'transparent'
+    },
+    mark: {
+        width: 180,
+        height: 200
+    },
+    signin: {
+        backgroundColor: '#FF3366',
+        padding: 20,
+        alignItems: 'center'
+    },
+    signup: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: .15
+    },
+    inputs: {
+        marginTop: 10,
+        marginBottom: 10,
+        flex: .25
+    },
+    inputPassword: {
+        marginLeft: 15,
+        width: 20,
+        height: 21
+    },
+    inputUsername: {
+      marginLeft: 15,
+      width: 20,
+      height: 20
+    },
+    inputContainer: {
+        padding: 10,
+        borderWidth: 1,
+        borderBottomColor: '#CCC',
+        borderColor: 'transparent'
+    },
+    input: {
+        position: 'absolute',
+        left: 61,
+        top: 12,
+        right: 0,
+        height: 20,
+        fontSize: 14
+    },
+    forgotContainer: {
+      alignItems: 'flex-end',
+      padding: 15,
+    },
+    greyFont: {
+      color: '#D8D8D8'
+    },
+    whiteFont: {
+      color: '#FFF'
+    }
+})
 
-AppRegistry.registerComponent('oursToDo', () => oursToDo);
+AppRegistry.registerComponent('oursToDo', () => login);
+
+module.exports = login;
---
+++ ios_views/common/home.js
@@ -0,0 +1,121 @@
+/**
+ * oursToDo
+ * https://github.com/yangxiaomu/oursToDo.git
+ */
+'use strict';
+
+var React = require('react-native');
+var Navigation = require('./navigation');
+var GroupToDo = require('./../group/todo_group_list');
+var Summary = require('./../summary/sum_group_list');
+var addGroup = require('./../group/add_group');
+
+var Icon = require("react-native-vector-icons/FontAwesome");
+
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TabBarIOS,
+  Navigator,
+  ScrollView,
+  NavigatorIOS,
+  StatusBarIOS,
+  AlertIOS
+} = React;
+
+StatusBarIOS.setHidden(true);
+var home = React.createClass({
+  getInitialState: function(){
+    AlertIOS.alert(this.prop.user_code)
+    
+    return {
+      selectedTab: GroupToDo
+    };
+  },
+
+  onRightButtonPress: function() {
+    this.refs.nav.push({
+      title: 'addGroup',
+      component: addGroup
+    })
+  },
+
+  _renderContent: function(category: string, title: ?string) {
+    if (title == "GroupToDo") {
+      return (
+        <NavigatorIOS style={styles.wrapper}
+          ref="nav"
+          tintColor='#DA552F'
+          barTintColor='#FFFFFD'
+          titleTextColor='#DA552F'
+          initialRoute={{
+            component: this.state.selectedTab,
+            title: title,
+            passProps: {filter: category},
+            rightButtonTitle: "➕",
+            onRightButtonPress: this.onRightButtonPress
+          }}
+        />
+      );
+    } else {
+      return (
+        <NavigatorIOS style={styles.wrapper}
+          initialRoute={{
+            component: this.state.selectedTab,
+            title: title,
+            passProps: {filter: category}
+          }}
+        />
+      );
+    }
+  },
+
+  render: function() {
+    return (
+      <TabBarIOS tintColor={"#ea4c89"}>
+
+        <Icon.TabBarItem
+          title="todo列表"
+          iconName="dribbble"
+          selectedIconName="dribbble"
+          selected={this.state.selectedTab == GroupToDo}
+          onPress={() => {
+            this.setState({
+              selectedTab: GroupToDo
+            });
+          }}>
+          {this._renderContent("GroupToDo", "GroupToDo")}
+
+        </Icon.TabBarItem>
+
+        <Icon.TabBarItem
+          title="统计"
+          iconName="lightbulb-o"
+          selectedIconName="lightbulb-o"
+          selected={this.state.selectedTab == Summary}
+          onPress={() => {
+            this.setState({
+              selectedTab: Summary
+            });
+          }}>
+          {this._renderContent("Summary", "Summary")}
+        </Icon.TabBarItem>
+        
+      </TabBarIOS>
+
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  }
+});
+
+module.exports = home;
+
+
